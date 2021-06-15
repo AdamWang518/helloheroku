66a1ea3fc3380f37c53d1025228f1d659db45145
AdamWang518 <65644807+AdamWang518@users.noreply.github.com>
2021-06-15 18:52:28 +0800

    Update DataBase.php

--- DataBase.php
+++ DataBase.php
@@ -1,8 +1,8 @@
 <?php
-$db_host='sql213.byethost.com';
-$db_name='b14_20529819_musicbox';
-$db_user='b14_20529819';
-$db_pass='wry877fuss291';
+$db_host='database-2.c3vujoluny2g.us-east-1.rds.amazonaws.com';
+$db_name='Final';
+$db_user='admin';
+$db_pass='mysql518';
 $dsn ="mysql:host=$db_host;dbname=$db_name;charset=utf8";
 $conn = new PDO($dsn, $db_user, $db_pass);
 ?>
